bd62c85a5ba7aebf5632b5e9a2495c340d3d7f76
Goodymind <aabuke9@gmail.com>
2024-11-25 19:38:21 +0800

    working find_vertical_seam

--- seamcarver.py
+++ seamcarver.py
@@ -2,6 +2,7 @@
 
 from picture import Picture
 from math import sqrt
+import sys
 
 class SeamCarver(Picture):
     ## TO-DO: fill in the methods below
@@ -9,56 +10,55 @@ class SeamCarver(Picture):
         '''
         Return the energy of pixel at column i and row j
         '''
-        # apparently (based on my guess), self[i,j] returns a tuple (r,g,b) for pixel [i,j]
-        x_0 = self[i-1,j] if (i-1, j) in self else (0,0,0)
-        x_2 = self[i+1,j] if (i+1, j) in self else (0,0,0)
-        y_0 = self[i, j-1] if (i, j-1) in self else (0,0,0)
-        y_2 = self[i, j+1] if (i, j+1) in self else (0,0,0)
-        xcdiff =  (x_2[0]-x_0[0])**2 + (x_2[1]-x_0[1])**2 + (x_2[2]-x_0[2])**2
-        ycdiff =  (y_2[0]-y_0[0])**2 + (y_2[1]-y_0[1])**2 + (y_2[2]-y_0[2])**2
-        return sqrt(xcdiff + ycdiff)
+        i0 = (i - 1 + self.width()) % self.width()
+        i2 = (i + 1 + self.width()) % self.width()
+        j0 = (j - 1 + self.height()) % self.height()
+        j2 = (j + 1 + self.height()) % self.height()
+        x_0 = self[i0, j]
+        x_2 = self[i2, j]
+        y_0 = self[i, j0]
+        y_2 = self[i, j2]
+        return sqrt(((x_2[0]-x_0[0])**2 + (x_2[1]-x_0[1])**2 + (x_2[2]-x_0[2])**2) + ((y_2[0]-y_0[0])**2 + (y_2[1]-y_0[1])**2 + (y_2[2]-y_0[2])**2))
 
     def find_vertical_seam(self) -> list[int]:
         '''
         Return a sequence of indices representing the lowest-energy
         vertical seam
         '''    
-        # +1 is a neat little trick
-        energies = [[0]* (self.width()) for _ in range(self.height())]
-        
+        costs = [[0]* (self.height()) for _ in range(self.width())]
         def smallest_triple(i,j):
             '''
             returns either i-1, i, or i+1 whoever is the smallest
             '''
-
-            mid = energies[i, j] if (i, j) in energies else 0
-            left = energies[i-1, j] if (i-1, j) in energies else mid + 1
-            right = energies[i+1, j] if (i+1, j) in energies else mid + 1
+            i0 = (i - 1 + self.width()) % self.width()
+            i2 = (i + 1 + self.width()) % self.width()
+            mid = costs[i][j]
+            left = costs[i0][j]
+            right = costs[i2][j]
             if left < mid and left < right:
-                return i-1
+                return i0
             if mid < left and mid < right:
                 return i
             if right < mid and right < left:
-                return i+1
+                return i2
             return i
-
-        
-        # get the energies
-        
+        # get the costs
         for j in range(self.height()):
+            smallest_cost_i = 0
+            smallest_cost = sys.maxsize
             for i in range(self.width()):
                 smallest_i = smallest_triple(i, j-1)
-                energies[i,j] = self.energy(i, j) + energies[smallest_i, j-1]
-                # no need to worry for first row case because [0-1] = [height+1] = 0
-        
+                costs[i][j] = self.energy(i, j) + costs[smallest_i][j-1]
+                if costs[i][j] < smallest_cost:
+                    smallest_cost = costs[i][j]
+                    smallest_cost_i = i
         # find the shortest cost path
-        # smallest_i now contains the index of the most recent path
+        # smallest_cost_i now contains the index of the smallest cost in the last row
         # we now iterate upwards
-        path = [energies[i,j]]
-        for j in range(self.height()-1, -1):
-            smallest_i = smallest_triple(smallest_i, j)
-            path.append(energies[smallest_i, j])
-        
+        path = [0] * self.height()
+        for j in range(self.height()-1, -1, -1):
+            smallest_cost_i = smallest_triple(smallest_cost_i, j)
+            path[j] = smallest_cost_i 
         return path
     def find_horizontal_seam(self) -> list[int]:
         '''
